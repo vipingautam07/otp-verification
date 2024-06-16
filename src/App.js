93c81363e345d3310162dd78c97d3039fc13f0c7
Vipin Gautam <95081432+vipingautam07@users.noreply.github.com>
2024-06-17 00:15:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ const App = () => {
           <><h2 className="text-center text-white font-medium text-2xl">
             👍Login Success
           </h2>
-          <a href="https://www.google.com">
+          <a href="https://www.rgpv.ac.in/Index.aspx">
             <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
               Click Here to continue!
             </button>
